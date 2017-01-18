a8d3acf7ccdc16ccb159587bd27f3d8ab76ac418
wangle <wangle@qiyi.com>
2017-01-19 00:19:07 +0800

    tj

--- src/main/java/com/example/myproject/action/HelloAction.java
+++ src/main/java/com/example/myproject/action/HelloAction.java
@@ -6,8 +6,11 @@ import com.example.myproject.service.UserService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Date;
 
@@ -27,13 +30,14 @@ public class HelloAction {
     @Autowired
     private HelloServiceProperties helloServiceProperties;
 
-    @RequestMapping("/")
-    String home() {
+    @RequestMapping("/hello")
+    public ModelAndView home(Model model) {
         logger.info(" helloServiceProperties = " + helloServiceProperties );
-
         User userInfo = userService.getUserInfo();
         logger.info(userInfo.getName() + " " + userInfo.getAge() );
-        return " 现在时间：" + (new Date()).toLocaleString() + " Hello World!" + "name = "+name + " age = " + age;
+        model.addAttribute("info"," 现在时间：" + (new Date()).toLocaleString() + " Hello World!" + "name = "+name + " age = " + age);
+        ModelAndView mv = new ModelAndView("hello");//如果使用@RestController注解，必须使用ModelAndView将返回的界面封装一下
+        return mv;
     }
 
     public void setName(String name) {
